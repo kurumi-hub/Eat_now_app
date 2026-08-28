8540c2493acd9c1d975acc41ad42160e793cb499
Tran The Bao <thebaoa1162003@gmail.com>
2026-08-28 16:51:25 +0700

    page update 62

--- src/components/restaurant/RestaurantDetailPage.tsx
+++ src/components/restaurant/RestaurantDetailPage.tsx
@@ -379,6 +379,7 @@ export default function RestaurantDetailPage({
                     </Link>
                     {item.isAvailable ? (
                       <IconButton
+                        type="button"
                         aria-label={`Thêm ${item.name}`}
                         className="restaurant-add-button"
                         onClick={(event) => {
