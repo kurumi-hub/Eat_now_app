5abc0cffc521749e5c9f1350bda23c8178c927b1
Tran The Bao <thebaoa1162003@gmail.com>
2026-09-05 00:56:33 +0700

    page upgrade 11

--- src/components/common/OrderPageHeader.tsx
+++ src/components/common/OrderPageHeader.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { Alert, Snackbar } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 import CustomerHeader from "@/components/home/CustomerHeader";
 import type { PublicUser } from "@/types/auth";
@@ -18,26 +16,11 @@ type OrderPageHeaderProps = {
  */
 export default function OrderPageHeader({ user }: OrderPageHeaderProps) {
   const router = useRouter();
-  const [notice, setNotice] = useState("");
 
   return (
-    <>
-      <CustomerHeader
-        user={user}
-        onPlaceholder={setNotice}
-        onSectionNavigate={(sectionId) => router.push(`/#${sectionId}`)}
-      />
-
-      <Snackbar
-        open={Boolean(notice)}
-        autoHideDuration={3500}
-        onClose={() => setNotice("")}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-      >
-        <Alert severity="info" onClose={() => setNotice("")}>
-          {notice}
-        </Alert>
-      </Snackbar>
-    </>
+    <CustomerHeader
+      user={user}
+      onSectionNavigate={(sectionId) => router.push(`/?home=1#${sectionId}`)}
+    />
   );
 }
